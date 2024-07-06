7959eaaf28beb478ebfe187bc7a31a17cb46b8ed
prabowo-sakti <prabowosakti2023@gmail.com>
2024-07-06 18:52:46 +0700

    commit pertama next js

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+  return (
+    <html lang="eng">
+      <head>
+        <title>Belajar Next JS fundamental</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
