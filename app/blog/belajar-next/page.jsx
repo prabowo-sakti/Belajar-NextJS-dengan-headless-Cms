2377fde4effcd8ec7b73d0d1d4a7a257d9e09e65
prabowo-sakti <prabowosakti2023@gmail.com>
2024-07-17 22:42:10 +0700

    menambahkan fitur markdown dan menambahkan library marked

--- app/blog/belajar-next/page.jsx
+++ app/blog/belajar-next/page.jsx
@@ -1,10 +1,15 @@
 import Heading from "@/components/Header";
+import { marked } from "marked";
+import { readFile } from "node:fs/promises";
 
-export default function PostPage() {
+export default async function PostPage() {
+  const text = await readFile("content/belajar-nextJS.md", "utf-8");
+  const html = marked(text);
   return (
     <>
       <Heading>Belajar Next.js</Heading>
-      <p>halaman dari blog</p>
+      <img src="/img/image.jpeg" alt="" />
+      <article dangerouslySetInnerHTML={{ __html: html }} />
     </>
   );
 }
