21ab5eb0dce8ea368ec909d825704ff0912f3424
prabowo-sakti <prabowosakti2023@gmail.com>
2024-07-30 18:21:46 +0700

    menampilkan postCard agar lebih dinamis

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,17 +1,30 @@
 import Heading from "@/components/Header";
+import SharelinkButton from "@/components/ShareLinkButton";
 import { getPost } from "@/lib/getPost";
+
+export async function generateMetadata({ params: { slug } }) {
+  const post = await getPost(slug);
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+}
 
 export default async function PostPage({ params: { slug } }) {
   const post = await getPost(slug);
-
   return (
     <>
       <Heading>{post.title}</Heading>
-      <p className="italic text-sm pb-2">
-        <b>
-          {post.date} - {post.author}
-        </b>
-      </p>
+
+      <div className="flex justify-between items-baseline gap-3 pb-2">
+        <p className="italic text-sm pb-2">
+          <b>
+            {post.date} - {post.author}
+          </b>
+        </p>
+        <SharelinkButton />
+      </div>
+
       <img src={post.image} alt="" />
       <article
         dangerouslySetInnerHTML={{ __html: post.body }}
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,9 +2,14 @@ import PostCard from "@/components/PostCard";
 import Heading from "../../components/Header";
 import { getAllPost } from "@/lib/getPost";
 
+export const metadata = {
+  title: "Blog Kumpulan Artikel",
+  description:
+    "kami menyediakan beberapa artikel berkualitas seputar teknologi, anime, dan coding",
+};
+
 export default async function BlogPage() {
   const posts = await getAllPost();
-  console.log(posts);
   return (
     <>
       <Heading>Blog</Heading>
